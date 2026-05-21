89da0d08466ec5f97ffcd5a8b4f0ae80fc4684f4
LoBarone <lorenzobar241@gmail.com>
2026-05-20 22:07:42 -0300

    correccion

--- tp2/ejer10.sh
+++ tp2/ejer10.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-if [ ! -f $HOME/Documents/Facu/OS/Tp1/usuarios ]; then
+if [ $# -ne 1 ]; then
+	echo "Debes pasar el nombre del usuario simulado como argumento"
+	exit
+fi
+
+if [ ! -f "$HOME/Documents/Facu/OS/Tp1/usuarios" ]; then
 	echo "No existe el archivo usuarios"
 	exit
 fi
 
-cp "$HOME/Documents/Facu/OS/Tp1/usuarios" usuarios #Copia el archivo al dir actual
+cp "$HOME/Documents/Facu/OS/Tp1/usuarios" usuarios | exit 1  #Copia el archivo al dir actual
 
-echo "usu01:x:uid:guid: :/home/usu01:/bin/bash" >> usuarios #Agrega la linea al final del archivo
+nombre=$1
+echo "$nombre:x:uid:guid: :/home/usu01:/bin/bash" >> usuarios #Agrega la linea al final del archivo
