967f3cb410cd27fd343b11c02c1b86112204677a
LoBarone <lorenzobar241@gmail.com>
2026-06-13 21:08:21 -0300

    algo

--- tp3/ejer12.sh
+++ tp3/ejer12.sh
@@ -1,6 +1,12 @@
 if [ $# -ne 2 ]; then
-	echo "Debes ingresar 2"
+	echo "Debes ingresar 2 strings"
 	exit 1
 fi
 
-
+if [[ $1 == $2 ]]; then
+	echo "cadenas correctas"
+	exit 0
+else
+	echo "cadenas incorrectas"
+	exit 1
+fi
